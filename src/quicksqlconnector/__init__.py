e0fdbf7925c73c33123a83712e7b52f7261daf99
anas-dew <mailtoanas@duck.com>
2022-05-11 12:26:27 +0530

    UPDATE 1.1

--- src/quicksqlconnector/__init__.py
+++ src/quicksqlconnector/__init__.py
@@ -1,41 +1,58 @@
-__version__ = '1.0.3'
+__version__ = '1.1'
 from typing import Optional
 import mysql.connector as mysql
+from prettytable import PrettyTable
 
-def contains_word(search_string, target_word):
+def contains_word(search_string, target_word): # TO MATCH SPECIFIC WORDS FROM QUERY TO PERFORM QUERY SPECIFIC TASKS
     return f'{target_word}' in f'{search_string}'
 
 class quicksqlconnector:
 
     def __init__(self, host:str, port:int, user:str, password:str, database = Optional[str]):
         
-        try:
+        try: # TRYING TO CONNECT TO DB IF DB NAME PROVIDED
 
             self.SQL = mysql.Connect(host=f'{host}', port=port, 
                             user=f'{user}', password=f'{password}', database=f'{database}')
             # print('current database : {}'.format(database))
-        except mysql.errors.ProgrammingError :
+
+        except mysql.errors.ProgrammingError : # IF DB NAME NOT FOUND CONNECT TO SERVER
             
             self.SQL = mysql.Connect(host=f'{host}', port=port, 
                             user=f'{user}', password=f'{password}')
             print('No database exists with name : {}'.format(database))
+            print('\nConnected to MySQL Server successfully.')
 
         except:
             raise ValueError
 
     def query(self, MyQuery:str):
-            
+
+        table = PrettyTable()
         try :
             
-            if contains_word(MyQuery, 'select') or contains_word(MyQuery, 'show') == True :
+            if contains_word(MyQuery, 'select')  == True :
 
                 sql_cursor = self.SQL.cursor()
                 sql_cursor.execute(MyQuery)
 
                 all_info = []
+
                 for bits_of_data in sql_cursor:
                     all_info.append(bits_of_data)
                 return all_info
+
+            elif contains_word(MyQuery, 'show') == True :
+
+                table.field_names = ['Table']
+                sql_cursor = self.SQL.cursor()
+                sql_cursor.execute(MyQuery)
+
+                # all_info = []
+                for bits_of_data in sql_cursor:
+                    # all_info.append(bits_of_data[0])
+                    table.add_row([bits_of_data[0]])
+                return table
 
             else :
 
@@ -48,29 +65,13 @@ class quicksqlconnector:
         except mysql.errors.ProgrammingError:
             print('Something bad with command')
         except mysql.errors.IntegrityError:
-                print('Primary Key Duplicate Error')
+            print('Primary Key Duplicate Error')
         except mysql.errors.InternalError:
             print('Unread result found')
         except :
             print("SQL Command Error")
 
 
-
 if __name__ == "__main__" :
     
-    DB = quicksqlconnector('localhost', 6606, 'root', 'anas9916','userbase')
-
-    # DB.query(f"insert into fb values({22},'Anas',{22},'Delhi')")
-    # print(DB.query('select * from fb where age=1'))
-    print(DB.query('select * from fb'))
-    # print(DB.query('use usrbase'))
-    # print(DB.query('show databases'))
-
-    # me = DB.query('select * from fb where age=1')
-    # print(me)
-
-    # print(DB.query('select * from fb where city like "%_w"'))
-    # print(DB.query('desc fb'))
-
-
     pass
