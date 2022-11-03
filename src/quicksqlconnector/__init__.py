816415f14060cabe5f9e15e89f840cfd8f0e1a66
anas-dew <mailtoanas@duck.com>
2022-11-03 08:46:02 +0530

    version 1.4 error msg changed few improvements

--- src/quicksqlconnector/__init__.py
+++ src/quicksqlconnector/__init__.py
@@ -1,37 +1,41 @@
-__version__ = '1.2'
+__version__ = '1.4'
+import csv
 from typing import Optional
 import mysql.connector as mysql
 from prettytable import PrettyTable
 
-def contains_word(search_string, target_word): # TO MATCH SPECIFIC WORDS FROM QUERY TO PERFORM QUERY SPECIFIC TASKS
+
+# TO MATCH SPECIFIC WORDS FROM QUERY TO PERFORM QUERY SPECIFIC TASKS
+def contains_word(search_string, target_word):
     return f'{target_word}' in f'{search_string}'
+
 
 class quicksqlconnector:
 
-    def __init__(self, host:str, port:int, user:str, password:str, database = Optional[str]):
-        
-        try: # TRYING TO CONNECT TO DB IF DB NAME PROVIDED
+    def __init__(self, host: str, port: int, user: str, password: str, database=Optional[str]):
 
-            self.SQL = mysql.Connect(host=f'{host}', port=port, 
-                            user=f'{user}', password=f'{password}', database=f'{database}')
+        try:  # TRYING TO CONNECT TO DB IF DB NAME PROVIDED
+
+            self.SQL = mysql.Connect(host=f'{host}', port=port,
+                                     user=f'{user}', password=f'{password}', database=f'{database}')
             # print('current database : {}'.format(database))
 
-        except mysql.errors.ProgrammingError : # IF DB NAME NOT FOUND CONNECT TO SERVER
-            
-            self.SQL = mysql.Connect(host=f'{host}', port=port, 
-                            user=f'{user}', password=f'{password}')
+        except mysql.errors.ProgrammingError:  # IF DB NAME NOT FOUND CONNECT TO SERVER
+
+            self.SQL = mysql.Connect(host=f'{host}', port=port,
+                                     user=f'{user}', password=f'{password}')
             print('No database exists with name : {}'.format(database))
             print('\nConnected to MySQL Server successfully.')
 
         except:
             raise ValueError
 
-    def query(self, MyQuery:str):
+    def query(self, MyQuery: str):
 
         table = PrettyTable()
-        try :
-            
-            if contains_word(MyQuery, 'select')  == True :
+        try:
+
+            if contains_word(MyQuery, 'select') == True:
 
                 sql_cursor = self.SQL.cursor()
                 sql_cursor.execute(MyQuery)
@@ -42,9 +46,9 @@ class quicksqlconnector:
                     all_info.append(bits_of_data)
                 return all_info
 
-            elif contains_word(MyQuery, 'show') == True :
+            elif contains_word(MyQuery, 'show') == True:
 
-                table.field_names = ['Table']
+                table.field_names = ['Result']
                 sql_cursor = self.SQL.cursor()
                 sql_cursor.execute(MyQuery)
 
@@ -54,24 +58,37 @@ class quicksqlconnector:
                     table.add_row([bits_of_data[0]])
                 return table
 
-            else :
+            else:
 
                 sql_cursor = self.SQL.cursor()
                 sql_cursor.execute(MyQuery)
                 self.SQL.commit()
 
                 return f'Query OK with command : {MyQuery}'
-            
+
         except mysql.errors.ProgrammingError:
             print('Command Error !')
         except mysql.errors.IntegrityError:
-            print('Primary Key Duplicate Error')
+            print('Duplicate primary key found !')
         except mysql.errors.InternalError:
             print('Unread result found')
         # except : // removed this to get more accurate, errors.
         #     print("SQL Command Error")
 
 
-if __name__ == "__main__" :
-    
+if __name__ == "__main__":
+    DB = quicksqlconnector('localhost', 6606, 'root', 'anas9916', 'userbase')
+    # print(DB.query('show databases'))
+    # # print(DB.query('use usrbase'))
+    # print(DB.query('show databases')[0][0])
+    # print(DB.query('show tables'))
+    # print(DB.query('show databases'))
+
+    # result = DB.query('select * from fb')
+    # c = csv.writer(open('dbdump01.csv', 'w'))
+    # for x in result:
+    #     c.writerow(x)
+
+    # print(DB.query('select * from fb where city like "%_i"'))
+    # print(DB.query('desc fb'))
     pass
