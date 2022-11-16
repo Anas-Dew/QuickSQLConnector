4a4d410386737481c479b1fa0df5bf3cd3e5fc85
anas-dew <mailtoanas@duck.com>
2022-11-16 21:01:16 +0530

    clear errors. bug fix

--- src/quicksqlconnector/__init__.py
+++ src/quicksqlconnector/__init__.py
@@ -1,23 +1,30 @@
-__version__ = '1.4.1'
+__version__ = '1.4.2'
 from typing import Optional
 import mysql.connector as mysql
 from prettytable import PrettyTable
 
 
-# TO MATCH SPECIFIC WORDS FROM QUERY TO PERFORM QUERY SPECIFIC TASKS
-def contains_word(search_string, target_word):
-    return f'{target_word}' in f'{search_string}'
-
-
 class quicksqlconnector:
 
     def __init__(self, host: str, port: int, user: str, password: str, database=Optional[str]):
+        """
+        It tries to connect to a database, if it fails it tries to connect to the server
+        
+        :param host: The hostname of the MySQL server
+        :type host: str
+        :param port: The port number to use when connecting to the MySQL server
+        :type port: int
+        :param user: The username to log in as
+        :type user: str
+        :param password: str, database=Optional[str]
+        :type password: str
+        :param database: Optional[str]
+        """
 
         try:  # TRYING TO CONNECT TO DB IF DB NAME PROVIDED
 
             self.SQL = mysql.Connect(host=f'{host}', port=port,
                                      user=f'{user}', password=f'{password}', database=f'{database}')
-            # print('current database : {}'.format(database))
 
         except mysql.errors.ProgrammingError:  # IF DB NAME NOT FOUND CONNECT TO SERVER
 
@@ -29,15 +36,22 @@ class quicksqlconnector:
         except:
             raise ValueError
 
-    def query(self, MyQuery: str):
+    def query(self, my_query: str):
+        """
+        It takes a query as a string, and returns the result of the query
+        
+        :param my_query: str
+        :type my_query: str
+        :return: The query is being returned.
+        """
 
         table = PrettyTable()
         try:
 
-            if contains_word(MyQuery, 'select') == True:
+            if 'select' in my_query:
 
                 sql_cursor = self.SQL.cursor()
-                sql_cursor.execute(MyQuery)
+                sql_cursor.execute(my_query)
 
                 all_info = []
 
@@ -45,41 +59,33 @@ class quicksqlconnector:
                     all_info.append(bits_of_data)
                 return all_info
 
-            elif contains_word(MyQuery, 'show') == True:
+            # elif contains_word(my_query, 'show') == True:
+            elif 'show' in my_query:
 
                 table.field_names = ['Result']
                 sql_cursor = self.SQL.cursor()
-                sql_cursor.execute(MyQuery)
+                sql_cursor.execute(my_query)
 
-                # all_info = []
                 for bits_of_data in sql_cursor:
-                    # all_info.append(bits_of_data[0])
                     table.add_row([bits_of_data[0]])
                 return table
 
             else:
 
                 sql_cursor = self.SQL.cursor()
-                sql_cursor.execute(MyQuery)
+                sql_cursor.execute(my_query)
                 self.SQL.commit()
+                return f'Query OK with command : {my_query}'
 
-                return f'Query OK with command : {MyQuery}'
-
-        except mysql.errors.ProgrammingError:
-            print('Command Error !')
-        except mysql.errors.IntegrityError:
-            print('Duplicate primary key found !')
-        except mysql.errors.InternalError:
-            print('Unread result found')
-        # except : // removed this to get more accurate, errors.
-        #     print("SQL Command Error")
+        except Exception as e:
+            print(e)
 
 
 if __name__ == "__main__":
     DB = quicksqlconnector('localhost', 6606, 'root', 'anas9916', 'userbase')
     # print(DB.query('show databases'))
-    # # print(DB.query('use usrbase'))
+    # DB.query('use usrbase')
     # print(DB.query('show databases')[0][0])
-    # print(DB.query('show tables'))
-    # print(DB.query('show databases'))
+    print(DB.query('show databases'))
+    # DB.query('select * from ')
     pass
