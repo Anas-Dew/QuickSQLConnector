93b8d923ca2eeff422cafcea20ae5c9f5efb673a
anas-dew <mailtoanas@duck.com>
2022-11-22 13:27:55 +0530

    Integration with sqlite3 and postgres done

--- src/quicksqlconnector/__init__.py
+++ src/quicksqlconnector/__init__.py
@@ -1,60 +1,97 @@
 __version__ = '1.4.42'
-from typing import Optional
 import mysql.connector as mysql
+import sqlite3
+import psycopg2
 
 class quicksqlconnector:
 
-    def __init__(self, database: str, host: str, port: int, user: str, password: str, database_name=Optional[str]):
+    def __init__(self, database: str = None, host: str = None, port: int = None, user: str = None, password: str = None, database_name=None):
         """
-        It tries to connect to a database, if it fails it tries to connect to the server
+        It connects to a database and returns a connection object
         
-        :param host: The hostname of the MySQL server
+        :param database: mysql, sqlite3, postgres
+        :type database: str
+
+        :param host: The hostname of the database server
         :type host: str
-        :param port: The port number to use when connecting to the MySQL server
+
+        :param port: The port number to connect to the database server
         :type port: int
+
         :param user: The username to log in as
         :type user: str
-        :param password: str, database=Optional[str]
+
+        :param password: The password of the user
         :type password: str
-        :param database: Optional[str]
+
+        :param database_name: The name of the database you want to connect to
+        :type database_name: str
         """
 
-        try:  # TRYING TO CONNECT TO DB IF DB NAME PROVIDED
+        self.database = database
+        self.host = host
+        self.port = port
+        self.user = user
+        self.password = password
+        self.database_name = database_name
 
-            self.SQL = mysql.Connect(host=f'{host}', port=port,
-                                     user=f'{user}', password=f'{password}', database=f'{database_name}')
-            self.SQL.autocommit = True
+        if self.database.lower() == 'mysql':
+            try:  # TRYING TO CONNECT TO DB IF DB NAME PROVIDED
 
-        except mysql.errors.ProgrammingError:  # IF DB NAME NOT FOUND CONNECT TO SERVER
+                self.SQL = mysql.Connect(host=f'{host}', port=port,
+                                         user=f'{user}', password=f'{password}', database=f'{database_name}')
+                self.SQL.autocommit = True
 
-            self.SQL = mysql.Connect(host=f'{host}', port=port,
-                                     user=f'{user}', password=f'{password}')
-            self.SQL.autocommit = True
-            print('No database exists with name : {}'.format(database_name))
-            print('\nConnected to MySQL Server successfully.')
+            except Exception as error:
+                print(error)
 
-        except:
-            raise ValueError
+        # EXPERIMENTAL AREA
+        elif self.database.lower() == 'sqlite3':
+            try:
+                self.SQL = sqlite3.connect(f'{self.database_name}.db')
+            except Exception as error:
+                print(error)
 
-
+        elif self.database.lower() == 'postgres':
+            try:
+                self.SQL = psycopg2.connect(
+                    host=self.host, database=self.database_name, user=self.user, password=self.password)
+                self.SQL.autocommit = True
+            except Exception as error:
+                print(error)
 
     def query(self, my_query: str, parameters=None):
+        """
+        It takes a query and parameters, and returns the result of the query
+        
+        :param my_query: The query you want to execute
+        :type my_query: str
+
+        :param parameters: A tuple of values to substitute in place of the parameters in the SQL
+        statement.
+
+        :return: A list of tuples which has data asked.
+        """
         packed_query = my_query.lower()
 
         try:
-            with self.SQL.cursor() as cursor:
-                    # IF YOU'VE BETTER IDEA HOW TO DEAL THIS, Open an issue: https://github.com/Anas-Dew/QuickSQLConnector/issues
-                    all_info = []
-                    if parameters:
-                        cursor.execute(packed_query, parameters)
-                    else:
-                        cursor.execute(packed_query)
+            # with self.SQL.cursor() as cursor: # Avoiding this because SQLite doesnt work with that.
+            cursor = self.SQL.cursor()
+            all_info = []
+            # IF YOU'VE BETTER IDEA HOW TO DEAL THIS, Open an issue: https://github.com/Anas-Dew/QuickSQLConnector/issues
+            if parameters:
+                cursor.execute(packed_query, parameters)
+                if self.database == 'sqlite3':
+                    self.SQL.commit()
+            else:
+                cursor.execute(packed_query)
+                if self.database == 'sqlite3':
+                    self.SQL.commit()
 
-                    for bits_of_data in cursor:
-                        all_info.append(bits_of_data)
+            for bits_of_data in cursor:
+                all_info.append(bits_of_data)
 
-                    cursor.close()
-                # print(f'Query OK with command : {packed_query}')
+            cursor.close()
             return all_info
 
         except Exception as e:
@@ -65,17 +102,30 @@ if __name__ == "__main__":
     # SOME TESTS WHICH I PERFORM WHILE CODING.
     # USE YOUR OWN CREDS WHEN CONTRIBUTING
 
-    DB = quicksqlconnector('mysql','localhost', 6606, 'root', 'anas9916', 'userbase')
+    # DB = quicksqlconnector('sqlite3','localhost', 6606, 'root', 'anas9916', 'userbase')
+    # DB = quicksqlconnector('mysql','localhost', 6606, 'root', 'anas9916', 'userbase')
+    # DB = quicksqlconnector('postgres','localhost', 5432, 'postgres', 'anas9916', 'test')
+
+
+    # MYSQL QUERY EXAMPLES
     # print(DB.query('show databases'))
     # DB.query('use userbase')
     # print(DB.query('show databases')[0][0])
     # print(DB.query('show tables'))
     # print(DB.query('SELECT * FROM new_fb '))
     # DB.query('CREATE TABLE test(name varchar(10), id int(10))')
-    # print(DB.query("INSERT INTO test values(%s, %s)", ('anas', 1)))
+    # print(DB.query("INSERT INTO test values(%s, %s)", ('anas', 154)))
     # DB.query('DROP TABLE test')
-    # print(DB.query('show tables'))
-    # input_user = '12 or 1=1' # SQL Injection Now Prevented
+    # input_user = '13 or 1=1' # SQL Injection Now Prevented
     # print(DB.query("SELECT * FROM test where id= %s", (input_user,)))
     # print(DB.query("SELECT * FROM test"))
+
+    # SQL LITE QUERY EXAMPLES
+    # DB.query('DROP TABLE movie')
+    # print(DB.query("CREATE TABLE movie(title varchar(1), year int(1), score int(1))"))
+    # print(DB.query("""
+    #     INSERT INTO movie VALUES
+    #         ('Monty Python and the Holy Grail', 1975, 8.2),
+    #         ('And Now for Something Completely Different', 1971, 7.5)
+    # """))
     pass
